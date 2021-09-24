c4228cd67441cf66acbfb478c701ab48f96671af
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2021-09-24 15:28:10 +0300

    Removed HTTPS redirect from WebStatus app

--- src/WebStatus/Startup.cs
+++ src/WebStatus/Startup.cs
@@ -56,7 +56,7 @@ namespace S2kDesignTemplate.WebStatus
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
 
             var pathBase = Configuration["PATH_BASE"];
@@ -71,7 +71,7 @@ namespace S2kDesignTemplate.WebStatus
                 config.AddCustomStylesheet(Path.Join("wwwroot", "css", "hc.css"));
             });
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
